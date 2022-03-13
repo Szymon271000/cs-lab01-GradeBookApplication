ea46aa11e6487d131bbe2170730c87ea2b2e893d
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-03-13 11:00:56 +0100

    StandardGradeBook class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,20 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook: BaseGradeBook
+    {
+        public string name;
+
+        public GradeBookType Type { get; set; }
+
+        public StandardGradeBook(string name): base(name)
+        {
+            this.name = name;
+            this.Type = GradeBookType.Standard;
+        }
+    }
+}
