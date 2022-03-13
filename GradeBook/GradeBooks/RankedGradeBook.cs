27fedab23f1ca782e7e6a25317660abe3d7984c4
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-03-13 11:06:46 +0100

    RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,20 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook: BaseGradeBook
+    {
+        public string name;
+
+        public GradeBookType Type { get; set; }
+
+        public RankedGradeBook(string name) : base(name)
+        {
+            this.name = name;
+            this.Type = GradeBookType.Ranked;
+        }
+    }
+}
