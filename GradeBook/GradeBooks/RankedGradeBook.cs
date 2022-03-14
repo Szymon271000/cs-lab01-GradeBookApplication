04b5c0f705b16f6a1952f59a5d35bbe63a04b18a
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-03-14 18:20:59 +0100

    ﻿CreateStatisticsOverridesTests

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,5 +16,29 @@ namespace GradeBook.GradeBooks
             this.name = name;
             this.Type = GradeBookType.Ranked;
         }
+
+        public override void CalculateStatistics()
+        {
+            if (base.Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students");
+            }
+            else if (base.Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name) 
+        {
+            if (base.Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students");
+            }
+            else if (base.Students.Count >= 5)
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
 }
